981881cf630b8a678d08b744b3b7a578eda6213f
Thomas Hall <thall1961@gmail.com>
2019-07-22 23:15:53 -0600

    initial

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,6 @@
-// let's go!
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./css/style.css";
+
+ReactDOM.render(<App />, document.getElementById("main"));
