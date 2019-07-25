014602f0b43d177592fd1c6c87ba5024808df63d
thall1961 <thall1961@gmail.com>
2019-07-25 09:05:56 -0600

    new api key setup

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -2,9 +2,9 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: '',
-    authDomain: '',
-    databaseURL: ''
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL
 });
 
 const base = Rebase.createClass(firebaseApp.database());
